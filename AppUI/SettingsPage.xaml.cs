05e8dfd21d20b8e5c4702f69253fd70d2dc4c7e5
sedefulker <sedefulker458@gmail.com>
2025-06-10 18:21:49 +0300

    #değişiklilik yapıldı

--- AppUI/SettingsPage.xaml.cs
+++ AppUI/SettingsPage.xaml.cs
@@ -1,26 +1,43 @@
+using Microsoft.Maui.Controls;
 using Microsoft.Maui.Storage;
 
 namespace AppUI
 {
-	public partial class SettingsPage : ContentPage
-	{
-		public SettingsPage()
-		{
-			InitializeComponent();
-			themeSwitch.IsToggled = Preferences.Get("dark_mode", false);
-			UpdateTheme(themeSwitch.IsToggled);
-		}
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+            
+            // Kaydedilmiþ tema tercihine göre switch'i ayarla (varsayýlan Light olarak false)
+            bool isDarkMode = Preferences.Get("dark_mode", false);
+            themeSwitch.IsToggled = isDarkMode;
+            
+            // Sayfanýn ve uygulamanýn temasýný bu deðere göre uygula.
+            ApplyTheme(isDarkMode);
+        }
 
-		private void OnThemeChanged(object sender, ToggledEventArgs e)
+		private void TemaSwitch_Toggled(object sender, ToggledEventArgs e)
 		{
 			Preferences.Set("dark_mode", e.Value);
-			UpdateTheme(e.Value);
+			App.Current.UserAppTheme = e.Value ? AppTheme.Dark : AppTheme.Light;
 		}
-
-		private void UpdateTheme(bool isDarkMode)
+		private void ApplyTheme(bool isDarkMode)
 		{
-			Application.Current.Resources["PageBackgroundColor"] = isDarkMode ? Color.FromArgb("#121212") : Colors.White;
-			Application.Current.Resources["LabelTextColor"] = isDarkMode ? Colors.White : Colors.Black;
+			if (isDarkMode)
+			{
+				Application.Current.Resources["PageBackgroundColor"] = Color.FromArgb("#121212");
+				Application.Current.Resources["LabelTextColor"] = Colors.White;
+				Application.Current.Resources["SecondaryLabelTextColor"] = Colors.Gray;
+			}
+			else
+			{
+				Application.Current.Resources["PageBackgroundColor"] = Colors.White;
+				Application.Current.Resources["LabelTextColor"] = Colors.Black; // Light mode için koyu renk
+				Application.Current.Resources["SecondaryLabelTextColor"] = Colors.DarkGray;
+			}
+
+			App.Current.UserAppTheme = isDarkMode ? AppTheme.Dark : AppTheme.Light;
 		}
 	}
-}
+    }
